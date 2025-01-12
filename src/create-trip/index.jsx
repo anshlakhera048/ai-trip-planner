3cfd9b999651a8e1a9f7a703ca52a32979264a9d
Ansh Lakhera <lakheraansh@gmail.com>
2025-01-12 19:24:33 +0530

    Adding UI Input

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function CreateTrip() {
+  const [place, setPlace]  = useState();
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
         <h2 className='font-bold text-3xl'>Tell us your Travel Preferences</h2>
@@ -12,6 +13,10 @@ function CreateTrip() {
             <h2 className='text-xl my-3 font-medium'>What is your Destination of your Choice?</h2>
             <GooglePlacesAutocomplete
                 apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+                selectProps={{
+                    place,
+                    onChange: (v)=>{setPlace(v); console.log(v)}
+                }}
             />
         </div>
     </div>
