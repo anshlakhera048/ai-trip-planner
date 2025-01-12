d2597dc57d633a968e7909ad60c56c770bb62c3a
Ansh Lakhera <lakheraansh@gmail.com>
2025-01-12 23:21:37 +0530

    New Working changes

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravellersList = [
+    {
+        id:1,
+        title: 'Just Me',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '😊',
+        people: '1'
+    },
+    {
+        id:2,
+        title: 'Couple',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '💕',
+        people: '2 people'
+    },
+    {
+        id:3,
+        title: 'Family',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '👨‍👩‍👧‍👦',
+        people: '3 to 5 people'
+    },
+    {
+        id:4,
+        title: 'Friends',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '🍟',
+        people: '5 to 10 people'
+    },
+]
+
+export const SelectBudgetOptions = [
+    {
+        id:1,
+        title: 'Cheap',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '💰',
+    },
+    {
+        id:2,
+        title: 'Moderate',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '💸',
+    },
+    {
+        id:3,
+        title: 'Luxury',
+        desc: 'All day all night mujhe yaad sataye teri',
+        icon: '🤑',
+    },
+]
+
+export const AI_Prompt = 'Generate a Travel Plan for Location : {location}' 
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import { Input } from '@/components/ui/input';
+import { SelectBudgetOptions } from '@/constants/options';
+import { SelectTravellersList } from '@/constants/options';
+import { Button } from '@/components/ui/button';
 
 function CreateTrip() {
   const [place, setPlace]  = useState();
@@ -24,7 +27,38 @@ function CreateTrip() {
                 <h2 className='text-xl my-3 font-medium'>Select number of Days :</h2>
                 <Input type="number" Placeholder={'Ex.3'}/>
             </div>
+
+            <div>
+                <h2 className='text-xl my-3 font-medium'>What is your Budget?</h2>
+                <div className='grid grid-cols-3 gap-5 mt-5'>
+                   {SelectBudgetOptions.map((item,index)=>(
+                    <div key={index} className=' p-4 cursor-pointer border rounded-lg hover:shadow-lg'>
+                        <h2 className='text-4xl'>{item.icon}</h2>
+                        <h2 className='font-bold text-lg'>{item.title}</h2>
+                        <h2 className='text-gray-500'>{item.desc}</h2>
+                    </div>
+                   ))}
+                </div>
+            </div>
+
+            <div>
+                <h2 className='text-xl my-4 font-medium'>Who's your Companion on your Trip?</h2>
+                <div className='grid grid-cols-3 gap-5 mt-5'>
+                   {SelectTravellersList.map((item,index)=>(
+                    <div key={index} className=' p-4 cursor-pointer border rounded-lg hover:shadow-lg'>
+                        <h2 className='text-4xl'>{item.icon}</h2>
+                        <h2 className='font-bold text-lg'>{item.title}</h2>
+                        <h2 className='text-gray-500'>{item.desc}</h2>
+                    </div>
+                   ))}
+                </div>
+            </div>
+
         </div>
+        <div className='my-3 justify-end flex'>
+           <Button>Generate Trip</Button>
+        </div>
+        
     </div>
   )
 }
