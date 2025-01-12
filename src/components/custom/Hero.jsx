82b6e0b23f62b93a92d7c78cd5eedd776c019854
Ansh Lakhera <lakheraansh@gmail.com>
2025-01-12 19:07:02 +0530

    More working changes

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center mx-56 gap-9'>
+        <h1
+            className='font-extrabold text-[50px] text-center mt-16'
+        ><span className='text-[#f56551]'>Discover Everything you're planning your Trip for</span> with AI Itenary Planner
+        </h1>
+        <p className='text-center text-xl text-gray-500'>
+        You used modular code (breaking down the application into small, reusable components) and clean architecture, allowing for easier addition of new features in the future without breaking the existing functionality.
+        </p>
+        
+        <Link to={'/create-trip'}>
+         <Button>Get started, it's Free</Button>
+        </Link>
+       
+    </div>
   )
 }
 
-export default Hero
+export default Hero 
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function CreateTrip() {
   return (
-    <div>CreateTrip Plan</div>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+        <h2 className='font-bold text-3xl'>Tell us your Travel Preferences</h2>
+        <p className='mt-3 text-gray-500 text-xl'>Just provide us some information on this with your Destination and budget preferences</p>
+    
+    <div className='mt-20'>
+        <div>
+            <h2 className='text-xl my-3 font-medium'>What is your Destination of your Choice?</h2>
+            <GooglePlacesAutocomplete
+                apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            />
+        </div>
+    </div>
+    </div>
   )
 }
 
